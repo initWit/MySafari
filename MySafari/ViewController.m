80d3631ff9eae16d30632ffa3635e977f1682e9e
initWit <figueras.robert@gmail.com>
2014-05-14 15:31:28 -0500

    Finished MVP

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate,UITextFieldDelegate>
+@property (strong, nonatomic) IBOutlet UIWebView *myWebView;
+@property (strong, nonatomic) IBOutlet UITextField *myURLTextField;
+@property (strong, nonatomic) IBOutlet UIButton *backButton;
+@property (strong, nonatomic) IBOutlet UIButton *forwardButton;
 
 @end
 
@@ -17,13 +21,69 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self.backButton setEnabled:NO];
+    [self.forwardButton setEnabled:NO];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSURL *myURL = [NSURL URLWithString:textField.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:myURL];
+    [self.myWebView loadRequest:request];
+    [textField resignFirstResponder];
+
+    return YES;
+
+}
+
+- (IBAction)onForwardButtonPressed:(id)sender
+{
+    [self.myWebView goForward];
+}
+
+- (IBAction)onBackButtonPressed:(id)sender
+{
+
+    [self.myWebView goBack];
+}
+
+- (IBAction)onStopLoadingButtonPressed:(id)sender
+
+{
+    [self.myWebView stopLoading];
+}
+
+- (IBAction)onReloadButtonPressed:(id)sender
+{
+    [self.myWebView reload];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+
+    if ([self.myWebView canGoForward])
+    {
+        [self.forwardButton setEnabled:YES];
+    }
+    else
+    {
+        [self.forwardButton setEnabled: NO];
+    }
+
+    if ([self.myWebView canGoBack])
+    {
+        [self.backButton setEnabled:YES];
+    }
+    else
+    {
+        [self.backButton setEnabled: NO];
+    }
 }
 
 @end
