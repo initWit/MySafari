440906df26364c8fe4989a3ff868ba2dc8cbca64
initWit <figueras.robert@gmail.com>
2014-05-14 23:22:32 -0500

    Fixed textfield fade and movement

--- MySafari/ViewController.h
+++ MySafari/ViewController.h
@@ -3,7 +3,6 @@
 //  MySafari
 //
 //  Created by Robert Figueras on 5/14/14.
-//  Copyright (c) 2014 AppSpaceship. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -3,7 +3,6 @@
 //  MySafari
 //
 //  Created by Robert Figueras on 5/14/14.
-//  Copyright (c) 2014 AppSpaceship. All rights reserved.
 //
 
 #import "ViewController.h"
@@ -28,52 +27,14 @@
     self.myWebView.scrollView.delegate = self;
 }
 
+- (IBAction)showMyAlert:(id)sender {
 
-- (void)viewDidAppear:(BOOL)animated{
-    [super viewDidAppear:animated];
-}
-
--(void)scrollViewDidScroll:(UIScrollView *)scrollView{
-
-    CGFloat myOffset = self.myWebView.scrollView.contentOffset.y;
-
-    NSLog(@"contentOffset.y is %g",(myOffset));
-
-    if (myOffset > 0) {
-        self.myURLTextField.alpha = (1-(myOffset*.01));
-
-
-        self.myURLTextField.frame = CGRectMake(20.0, 80.0 -myOffset, self.myURLTextField.frame.size.width, self.myURLTextField.frame.size.height);
-
-    }
-
-}
-
-
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField
-{
-
-    NSString *finishedURL = textField.text;
-
-
-    if (![textField.text hasPrefix:@"http://"]) {
-        finishedURL = [@"http://" stringByAppendingString:textField.text];
-    }
-
-    NSURL *myURL = [NSURL URLWithString:finishedURL];
-    NSURLRequest *request = [NSURLRequest requestWithURL:myURL];
-    [self.myWebView loadRequest:request];
-    [textField resignFirstResponder];
-
-    return YES;
-
+    UIAlertView *myAlertView = [[UIAlertView alloc]initWithTitle:@"Coming soon!"
+                                                         message:@"Coming soon!"
+                                                        delegate:self
+                                               cancelButtonTitle:@"OK"
+                                               otherButtonTitles:nil];
+    [myAlertView show];
 }
 
 - (IBAction)onForwardButtonPressed:(id)sender
@@ -98,6 +59,7 @@
     [self.myWebView reload];
 }
 
+#pragma mark - UIWebViewDelegate Methods
 
 -(void)webViewDidStartLoad:(UIWebView *)webView
 {
@@ -108,48 +70,83 @@
 {
 
     if ([self.myWebView canGoForward])
-    {
-        [self.forwardButton setEnabled:YES];
-    }
+        {
+            [self.forwardButton setEnabled:YES];
+        }
     else
-    {
-        [self.forwardButton setEnabled: NO];
-    }
+        {
+            [self.forwardButton setEnabled: NO];
+        }
 
     if ([self.myWebView canGoBack])
-    {
-        [self.backButton setEnabled:YES];
-    }
+        {
+            [self.backButton setEnabled:YES];
+        }
     else
-    {
-        [self.backButton setEnabled: NO];
-    }
+        {
+            [self.backButton setEnabled: NO];
+        }
 
-//    NSLog(@" is %@",webView.request.URL.absoluteString);
+
+    // *** set the navigation title as the website title
+
     self.myURLTextField.text = webView.request.URL.absoluteString;
-
     NSString *titleFromTheWebViewString = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
-
     self.navigationItem.title = titleFromTheWebViewString;
+
+    // *** stop spinning only when webview is done loading
 
     if (![webView isLoading]) {
         [self.mySpinner stopAnimating];
     }
 
+    // *** refresh the UITextField every time a new page is loaded
 
-
+    self.myURLTextField.frame = CGRectMake(20.0, 80.0, self.myURLTextField.frame.size.width, self.myURLTextField.frame.size.height);
+    self.myURLTextField.alpha = 1.0;
 
 }
 
-- (IBAction)showMyAlert:(id)sender {
+#pragma mark - UIScrollViewDelegate Methods
 
-    UIAlertView *myAlertView = [[UIAlertView alloc]initWithTitle:@"Coming soon!"
-                                                         message:@"Coming soon!"
-                                                        delegate:self
-                                               cancelButtonTitle:@"OK"
-                                               otherButtonTitles:nil];
-    [myAlertView show];
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+
+    CGFloat myOffset = (self.myWebView.scrollView.contentOffset.y + 64.0); // *** adjust scrollview offset for nav controller 64 px
+
+    int fadeRate = 30; // *** the lower the fadeRate, the faster the fade
+
+    if (myOffset > 0) {
+        self.myURLTextField.alpha = (1-((myOffset)/fadeRate)); // *** fade textfield based on position of scrollview and fadeRate
+        self.myURLTextField.frame = CGRectMake(20.0, (80.0 - myOffset), self.myURLTextField.frame.size.width, self.myURLTextField.frame.size.height);
+    }
+    else { // *** if user springs scrollview below 0, reset textfield position and alpha
+        self.myURLTextField.frame = CGRectMake(20.0, 80.0, self.myURLTextField.frame.size.width, self.myURLTextField.frame.size.height);
+        self.myURLTextField.alpha = 1.0;
+    }
+
 }
+
+#pragma mark - UITextFieldDelegate Methods
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+
+    NSString *finishedURL = textField.text;
+
+    if (![textField.text hasPrefix:@"http://"]) {
+        finishedURL = [@"http://" stringByAppendingString:textField.text];
+    }
+
+    NSURL *myURL = [NSURL URLWithString:finishedURL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:myURL];
+    [self.myWebView loadRequest:request];
+    [textField resignFirstResponder];
+    
+    return YES;
+    
+}
+
 
 
 @end
